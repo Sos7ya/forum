ad0a841e4a6043e96f48836a658356bf7cd62b4f
Sos7ya <k.moskal9901@gmail.com>
2023-03-09 21:02:55 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,67 @@
+let comments = [];
+loadComments();
+
+document.getElementById('comment-add').onclick = function FormWork(){
+    event.preventDefault();
+    let commentName = document.getElementById('comment-name');
+    let commentBody = document.getElementById('comment-body');
+
+
+    if(!commentName.value){
+        let error = document.getElementById('noName');
+        let errorText = '<p>ERROR: You cannot leave comment without a name!</p> ';
+
+        return error.innerHTML = errorText;
+    }
+
+    if(!commentBody.value){
+        let error = document.getElementById('noComment');
+        let errorText = '<p>ERROR: You cannot leave empty comment</p> ';
+        
+        return error.innerHTML = errorText;
+    }
+
+
+        let date = new Date()
+        let options = {
+            day: 'numeric',
+            month: 'numeric',
+            year: 'numeric'
+        }
+
+        let comment = {
+            name: commentName.value,
+            body: commentBody.value,
+            time: date.toLocaleDateString('ru', options)
+        }
+
+        commentName = " ";
+        commentBody = " ";
+
+        comments.push(comment);
+
+        saveComments();
+        showComments();
+
+    }
+
+function saveComments(){
+    localStorage.setItem('comments', JSON.stringify(comments));
+}
+
+function loadComments(){
+    if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
+    showComments();
+}
+
+function showComments(){
+    let commentField = document.getElementById('comment-field');
+    let out = ' ';
+
+    comments.forEach(function(item){
+        out += `<p class="time"><em>${item.time}</em></p>`;
+        out += `<h3 class="user-name">${item.name + ':'}</h3>`;
+        out += `<p class="user-comment">${item.body}</p>`;
+    })
+    commentField.innerHTML = out;
+}
